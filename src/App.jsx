55a4d08aaad3a3d4dbdf94c23093b06243eeab8e
tungnt <thanhtung3523@gmail.com>
2024-12-16 00:00:19 +0700

    Config mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <p>Nguyen Thanh Tung</p>
+
+      <Typography variant="body2" color="text.secondary">
+        Test text Test text
+      </Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
